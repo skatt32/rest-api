d6f104584501f7cec3d0642f1685099945d06ae3
Neto test <test@neto.com>
2024-02-15 15:55:46 +0400

    Initial commit

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -12,7 +12,7 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body(text)
+                .body("hi")
                 .when()
                 .post("/post")
                 .then()
